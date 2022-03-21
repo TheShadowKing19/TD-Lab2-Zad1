14d283ed6daf99395e657cd4f3c1c05ddad60bdd
TheShadowKing19 <mikiwolf19@gmail.com>
2022-03-21 15:04:42 +0100

    Skończenie zadania

--- main.py
+++ main.py
@@ -1,6 +1,35 @@
 import numpy as np
-import matplotlib as plt
+import matplotlib.pyplot as plt
+
 
 # Funkcja numer 8
 
+# Parametr f - częstość próbkowania
 f = 2
+
+T = 1 / f
+
+# Częstotliwość próbkowania
+fs = 8000
+
+# Okres próbkowania
+Ts = 1/fs
+
+# Czas trwania sygnału
+Tc = 1
+
+# Liczba próbek przypadających na cały sygnał
+N = Tc * fs
+
+# Wartość fi
+fi = 2 * np.pi
+
+t = np.arange(0, (N - 1) / fs, Ts)
+
+# Funkcja
+x = (1 - t) * np.sin(2 * np.pi * f * t + fi) * np.cos(4 * np.pi * t)
+
+plt.plot(t, x, label='x(t)')
+plt.title('Zad 1')
+plt.savefig('TD-Lab2_Zad1.png')
+plt.show()
